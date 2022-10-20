89e461c5776f0fa23160963c1134d899c8f16ae5
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-20 19:23:02 -0400

    test

--- js/script.js
+++ js/script.js
@@ -6,6 +6,48 @@
 
 "use strict"
 /**
+ * This function injects the function that calculates the area of a triangle
+ */
+
+function startClicked() {
+  document.getElementById("height-numeric-textfeild").innerHTML =
+  "<form action='#'>
+  <p>Base in cm</p>
+    <div class='mdl-textfield mdl-js-textfield'>
+      <input
+        class='mdl-textfield__input'
+        type='text'
+        pattern='-?[0-9]*(\.[0-9]+)?'
+        id='base-of-triangle'
+      />
+      <label class='mdl-textfield__label' for='base-entered'
+        >Base here ...</label
+      >
+      <span class='mdl-textfield__error'>Input is not a number!</span>
+    </div>
+  </form>
+  document.getElementById("perimeter").innerHTML = 
+  "<form action='#'>
+    <p>Height in cm</p>
+    <div class='mdl-textfield mdl-js-textfield'>
+      <input
+        class='mdl-textfield__input'
+        type='text'
+        pattern='-?[0-9]*(\.[0-9]+)?'
+        id='height-of-triangle'
+      />
+      <label class='mdl-textfield__label' for='height-entered'
+        >Height here ...</label
+      >
+      <span class='mdl-textfield__error'>Input is not a number!</span>
+    </div>
+  </form>"
+  
+  document.getElementById("").innerHTML = "<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick="enterClicked()"
+>Enter</button>"
+}
+
+/**
  * This function calculates the area of a triangle
  */
 function enterClicked() {
