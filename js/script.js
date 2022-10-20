b81736bf0b4ec060debc2041d4ae1193ccba8b7f
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-20 15:58:49 -0400

    init

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-// Copyright (c) 2022 Timothy Manwell All right reserved
+// Copyright (c) 2022 Timothy Manwell All rights reserved
 //
-//Created by: Timothy Manwell
-//Created on: Oct 2022
-//// This file contains the JS functions for index.html
+// Created by: Timothy Manwell
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
 
+"use strict"
 /**
- * This function displays "Hello,World!"
- * @constructor
+ * This function calculates the area of a triangle
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function enterClicked() {
+  //input
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+  const height = parseInt(document.getElementById("height-of-triangle").value)
+
+  //process
+  const area = (base * height) / 2
+
+  //output
+  document.getElementById("area-of-triangle").innerHTML =
+    "The area of the triangle is " + area + " cm²."
 }
+
